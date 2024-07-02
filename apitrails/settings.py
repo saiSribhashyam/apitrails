3ab7a785e057959bbc7cf7a0af01c4952c393f5d
Anantha Sai S <110450908+saiSribhashyam@users.noreply.github.com>
2024-07-02 12:02:49 +0530

    Update settings.py

--- apitrails/settings.py
+++ apitrails/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1^4t&=&1kviartdzs3c^9%vf037c9+o9n8n3qxfcxf%^my3t)r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
